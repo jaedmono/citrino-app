b930df1d0e7a66c8fd2c1d5ae580ddb7b330618a
x233370 <jaime.montealegreolivares@wnco.com>
2019-05-14 17:08:32 -0500

    adding layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FullComponent } from './full/full.component';
+import { AppHeaderComponent } from './full/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FullComponent,
+    AppHeaderComponent
   ],
   imports: [
     BrowserModule,
